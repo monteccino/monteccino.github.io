0c38f1c0cb6e9a3044688bf1be7b6aa66d9e7b5f
camila monteccino <36300765+monteccino@users.noreply.github.com>
2018-11-29 07:25:42 -0300

    Add files via upload

--- admin/cadastro.php
+++ admin/cadastro.php
@@ -1,31 +1,31 @@
 <?php 
- 
+ /**está pegando os dados de login, senha e entrar**/
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
-$connect = mysql_connect('localhost','root','');
-$db = mysql_select_db('tccstando');
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
+$connect = mysqli_connect('localhost','root','digitesenha','tccstando');  /**local q está o banco, nome root, senha do root, e nome do banco**/
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'"; /**selecionando qual tabela usar**/
+$select = mysqli_query($connect, $query_select); /**selecionando**/
+$array = mysqli_fetch_array($select);
 $logarray = $array['login'];
  
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+  if($login == "" || $login == null || $senha == "" || $senha == null ) /**caso algum campo esteja nulo ou em branco, vai mostrar essa mensagem, isso é bom pq n vai ter nada em branco**/{ 
+    echo"<script language='javascript' type='text/javascript'>alert('Um campo deve ser preenchido');window.location.href='cadastro.html';</script>";
  
     }else{
-      if($logarray == $login){
+      if($logarray == $login){ 
  
-        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>"; /** se já tiver um usuario cadastrado com o mesmo login, ele vai avisar q já existe e depois voltar para a página de cadastro**/
         die();
  
       }else{
-        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-        $insert = mysql_query($query,$connect);
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')"; /** aqui ele está adicionando os valores que foi pedido**/
+        $insert = mysqli_query($connect, $query);
          
         if($insert){
-          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
+          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>"; /** mensagem informando que foi inserido corretamente**/
+        }
+        else{
+          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>"; /** caso deu algum erro no banco, vai mostrar essa mensagem**/
         }
       }
     }
--- admin/login.php
+++ admin/login.php
@@ -1,18 +1,18 @@
 <?php 
-  $login = $_POST['login'];
+/**está pegando os dados de login, senha e entrar**/
+  $login = $_POST['login']; 
   $entrar = $_POST['entrar'];
   $senha = md5($_POST['senha']);
-  $connect = mysql_connect('localhost','root','');
-  $db = mysql_select_db('tccstando');
+  $connect = mysqli_connect('localhost','root','digitesenha','tccstando'); /**local q está o banco, nome root, senha do root, e nome do banco**/
     if (isset($entrar)) {
              
-      $verifica = mysql_query("SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die("erro ao selecionar");
-        if (mysql_num_rows($verifica)<=0){
-          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
+      $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die("erro ao selecionar"); /**aqui estou selecionando a tabela com o login e a senha, se não tiver, ele vai dar erro**/
+        if (mysqli_num_rows($verifica)<=0){
+          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>"; /**se não encontrar, irá dar um alerta falando que está incorreto**/
           die();
         }else{
-          setcookie("login",$login);
-          header("Location:index.php");
+          setcookie("login",$login); /** quando der certo, ele vai enviar direto para págino do admin**/
+          header("Location:admin.php");
         }
     }
 ?>
